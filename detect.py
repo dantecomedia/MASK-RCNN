c33e2b087b68f8c425c7da6ceb5d565c897c0a68
rahul <rahul.cse.ccu@gmail.com>
2019-09-03 15:30:50 +0530

    retieving the annotation

--- detect.py
+++ detect.py
@@ -1,1 +1,39 @@
 
+#https://machinelearningmastery.com/how-to-train-an-object-detection-model-with-keras/
+
+
+
+#extracting the bounding boxes
+
+
+from xml.etree import ElementTree
+
+def extract_bounding_boxes(filename):
+	tree= ElementTree.parse(filename)
+	root=tree.getroot()
+	boxes=list()
+	for box in root.findall('.//bndbox'):
+		xmin=int(box.find('xmin').text)
+		ymin=int(box.find('ymin').text)
+		xmax=int(box.find('xmax').text)
+		ymax=int(box.find('ymax').text)
+		coors=[xmin,ymin,xmax,ymax]
+		boxes.append(coors)
+	width=int(root.find(".//size/width").text)
+	height=int(root.find(".//size/height").text)
+	return boxes,width,height
+
+
+boxes,width,height=extract_bounding_boxes("/mnt/F8F8B8AFF8B86E0E/kangaroo-master/annots/00002.xml")
+print(boxes,width,height)
+
+class Kangroo(dataset):
+	def load_dataset(self,dataset_dir,is_train=True):
+
+
+
+	def load_mask(self, image_id):
+
+
+
+	def image_reference(self, image_id):
